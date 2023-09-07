c46085750d47ec824abfcb7d73664c21d95260cb
Chetan070901 <104587624+Chetan070901@users.noreply.github.com>
2023-09-07 08:44:55 +0000

    some more operation added

--- 01_basics/03_Conversion_Confusion.js
+++ 01_basics/03_Conversion_Confusion.js
@@ -1,13 +1,13 @@
-let score = "hitesh"
+// let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let value_Number = Number(score) // using this we can define a value to number whether it is a string or anything.
+// let value_Number = Number(score) // using this we can define a value to number whether it is a string or anything.
 
-console.log(typeof (value_Number));
+// console.log(typeof (value_Number));
 
-console.log(value_Number); // even though we declare the score to number but actual type of value_Number is NAN (Not A Number)
+// console.log(value_Number); // even though we declare the score to number but actual type of value_Number is NAN (Not A Number)
 
 // "50" => easily convert in number 50
 // "50abc" => not a number
@@ -22,18 +22,39 @@ console.log(value_Number); // even though we declare the score to number but act
 // console.log(boolean_Logged_In);
 
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber); // Use typeof, not tyoeof
-
-
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber); // Use typeof, not tyoeof
 
 
+// ############### Operations ######################
+
+// let value = 3
+// let neg_value = -value
+// console.log(neg_value);
+// console.log(typeof (neg_value));
+
+// console.log(2+2); // addition
+// console.log(2-2); // subtraction
+// console.log(2*2); // multiplication
+// console.log(2**2); // 2 ki power 2
+// console.log(2/2); // devision
+// console.log(2%2); // percentage
 
 
+// let str1 ="Hello"
+// let str2 =" World"
 
+// let str3 = str1 + str2
+// console.log(str3)
 
+let num1, num2, num3
 
+num1 = 23
+num2 = 33
+num3 = 44
+
+console.log(num1)
 
