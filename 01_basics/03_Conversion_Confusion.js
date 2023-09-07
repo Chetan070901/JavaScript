e0c1c4fb9b8597a019177557ab1ffc7d8ea8ecfa
Chetan070901 <104587624+Chetan070901@users.noreply.github.com>
2023-09-07 06:31:43 +0000

    Conversion of data type

---
+++ 01_basics/03_Conversion_Confusion.js
@@ -0,0 +1,39 @@
+let score = "hitesh"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value_Number = Number(score) // using this we can define a value to number whether it is a string or anything.
+
+console.log(typeof (value_Number));
+
+console.log(value_Number); // even though we declare the score to number but actual type of value_Number is NAN (Not A Number)
+
+// "50" => easily convert in number 50
+// "50abc" => not a number
+// true => 1; false => 0
+// "" value false
+// "chetan" value true
+
+
+// let logged_In = "0"
+
+// let boolean_Logged_In = Boolean(logged_In)
+// console.log(boolean_Logged_In);
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber); // Use typeof, not tyoeof
+
+
+
+
+
+
+
+
+
+
