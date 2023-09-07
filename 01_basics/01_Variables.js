db61f66442eb444d820d2ad5db6d108cbf8df544
Chetan070901 <104587624+Chetan070901@users.noreply.github.com>
2023-09-07 05:53:06 +0000

    This is my second commit and in this i added datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const account_Id = 99909
+let account_Email = "chetan@google.com"
+var account_Password = "Password"
+account_City = "Delhi"
+let account_State;
+
+// account_Id = "111111" // This is not working because we define const value only once in a specific code
+
+account_Email ="chetan@bikano.com"
+account_Password = "chetan"
+account_City = "Agra"
+
+
+
+
+
+console.table ([account_Id, account_Email, account_City, account_Password, account_State])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all js code as newer version
+
+// alert ("hello") We are using nodejs, not browser thatswhy this is not showing and we comment out this will work on later when we know this.
+
+console.log (99597*798789); // Code readebility should be high 
+
+console.log("chetna practice");
+
+
+let age = 18
+let name = "chetan"
+
+// Notes
+
+// number => 2 to power 53
+// bigint => This is used for big websites like facebook or any stock market website.
+// string => "" Whenever using string write in the quotation marks.
+// boolean => true/false
+// null => standalone value means empty output.
+// undefined => means the value is not defiened yet.
+// symbol => used for unique value.
+// object
+
+
+
+console.log (typeof undefined); // undefined
+console.log (typeof null); //  object
