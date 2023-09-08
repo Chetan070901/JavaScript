26f081da5bc206ad1b2e53b170712c66bc604028
Chetan070901 <104587624+Chetan070901@users.noreply.github.com>
2023-09-08 05:39:12 +0000

    added string and work on this later

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,16 @@
+const name ="chetan"
+const number = 9990923379
+
+// console.log(name + number + " This is the added value"); This is the old school method.
+
+// This is new school method
+
+// console.log( 'Hello my name is ${name} and my number is ${number} please contact me on my number') practice error.
+
+console.log(`Hello my name is ${name} and this is my number ${number} you can call me on this number.`)
+
+// const gameNmae = new String('chetan')
+
+const mystring = "table of content";
+
+document.body.innerHTML = mystring.anchor("contents_anchor");
