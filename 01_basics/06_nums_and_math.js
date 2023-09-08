5633519910e6794e97a53e190e9684e2ab6ec6ea
Chetan070901 <104587624+Chetan070901@users.noreply.github.com>
2023-09-08 06:21:23 +0000

    added math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const balance = new Number(400)
+
+const score = 100
+
+// console.log(balance);
+// console.log(score);
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(78, 78, 878, 1));
+// console.log(Math.max(78, 78, 878, 1));
+
+
+// console.log(Math.random());
+// console.log(Math.round(Math.random()*10 +1));
+
+const max = 6
+const min = 1
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
